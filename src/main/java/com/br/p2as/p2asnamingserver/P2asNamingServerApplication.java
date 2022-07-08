294eb4c50bceb77b987de5d05ff55f223a547330
Wellington <wellington.vasconcelos@gmail.com>
2022-07-08 15:28:20 -0300

    inicio de projeto

---
+++ src/main/java/com/br/p2as/p2asnamingserver/P2asNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.br.p2as.p2asnamingserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class P2asNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(P2asNamingServerApplication.class, args);
+	}
+
+}
