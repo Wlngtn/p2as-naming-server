baa3b3c7817b8c2820f3708d226df51a1d26b7ad
Wellington <wellington.vasconcelos@gmail.com>
2022-07-11 20:34:18 -0300

    Add config service properties

--- src/main/java/com/br/p2as/P2asNamingServerApplication.java
+++ src/main/java/com/br/p2as/P2asNamingServerApplication.java
@@ -1,8 +1,10 @@
-package com.br.p2as.p2asnamingserver;
+package com.br.p2as;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
+@EnableEurekaServer
 @SpringBootApplication
 public class P2asNamingServerApplication {
 
